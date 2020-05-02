05bd242963395c7a0b41fe6d2b593553dd628554
arsmn <arsmn1996@gmail.com>
2020-05-02 18:22:28 +0430

    Add tests for RequiresRoles

--- main_test.go
+++ main_test.go
@@ -105,7 +105,111 @@ func Test_RequiresPermission(t *testing.T) {
 
 		t.Run(tt.name, func(t *testing.T) {
 			req, _ := http.NewRequest("POST", "/blog", nil)
-			resp, err := app.Test(req, 50000)
+			resp, err := app.Test(req)
+			if err != nil {
+				t.Fatalf(`%s: %s`, t.Name(), err)
+			}
+
+			if resp.StatusCode != tt.statusCode {
+				t.Fatalf(`%s: StatusCode: got %v - expected %v`, t.Name(), resp.StatusCode, tt.statusCode)
+			}
+		})
+
+	}
+}
+
+func Test_RequiresRoles(t *testing.T) {
+
+	tests := []struct {
+		name        string
+		subLookupFn SubjectLookupFunc
+		roles       []string
+		rule        func(o *Options)
+		statusCode  int
+	}{
+		{
+			name:        "alice have user role",
+			subLookupFn: subjectAlice,
+			roles:       []string{"user"},
+			rule:        MatchAll,
+			statusCode:  200,
+		},
+		{
+			name:        "alice have admin role",
+			subLookupFn: subjectAlice,
+			roles:       []string{"admin"},
+			rule:        AtLeastOne,
+			statusCode:  200,
+		},
+		{
+			name:        "alice have both user and admin roles",
+			subLookupFn: subjectAlice,
+			roles:       []string{"user", "admin"},
+			rule:        MatchAll,
+			statusCode:  200,
+		},
+		{
+			name:        "alice have both user and admin roles",
+			subLookupFn: subjectAlice,
+			roles:       []string{"user", "admin"},
+			rule:        AtLeastOne,
+			statusCode:  200,
+		},
+		{
+			name:        "bob have only user role",
+			subLookupFn: subjectBob,
+			roles:       []string{"user"},
+			rule:        AtLeastOne,
+			statusCode:  200,
+		},
+		{
+			name:        "unauthenticated user have no permissions",
+			subLookupFn: subjectNil,
+			roles:       []string{"user"},
+			rule:        MatchAll,
+			statusCode:  401,
+		},
+		{
+			name:        "bob have not admin role",
+			subLookupFn: subjectBob,
+			roles:       []string{"admin"},
+			rule:        MatchAll,
+			statusCode:  403,
+		},
+		{
+			name:        "bob have only user role",
+			subLookupFn: subjectBob,
+			roles:       []string{"admin", "user"},
+			rule:        AtLeastOne,
+			statusCode:  200,
+		},
+		{
+			name:        "invalid role",
+			subLookupFn: subjectBob,
+			roles:       []string{"unknown"},
+			rule:        MatchAll,
+			statusCode:  403,
+		},
+	}
+
+	for _, tt := range tests {
+		app := *fiber.New()
+		authz := New(Config{
+			ModelFilePath: "./example/model.conf",
+			PolicyAdapter: fileadapter.NewAdapter("./example/policy.csv"),
+			SubLookupFn:   tt.subLookupFn,
+		})
+
+		app.Post("/blog",
+			authz.RequiresRoles(tt.roles, tt.rule),
+			func(c *fiber.Ctx) {
+				c.SendStatus(200)
+			},
+		)
+
+		t.Run(tt.name, func(t *testing.T) {
+			req, _ := http.NewRequest("POST", "/blog", nil)
+			resp, err := app.Test(req)
 			if err != nil {
 				t.Fatalf(`%s: %s`, t.Name(), err)
 			}
